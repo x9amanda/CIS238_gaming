2fc29c01df2bf816611859f30cfad214aa4a9edd
Amanda Dockray <amandadockray@gmail.com>
2024-02-27 12:34:07 -0500

    added mouse tracker and made something happen w/ certain position

--- game/Game.cpp
+++ game/Game.cpp
@@ -1,58 +1,74 @@
 #include "Game.h"
 
+// #define MAX_WIDTH 1024  // Max x resolution value
+// #define MAX_HEIGHT 768 // Max y resolution value
+// #define TW_THICKNESS 20 // Top wall thickness
+// #define BW_THICKNESS 20 // Bottom wall thickness
+
+bool flag = true;
+
 Game::Game()
-  : mWindow(nullptr),
-    mRenderer(nullptr),
-    mTicksCount(0),
-    mIsRunning (true)
-{ }
+    : mWindow(nullptr),
+      mRenderer(nullptr),
+      mTicksCount(0),
+      mIsRunning(true)
+{
+}
 
 bool Game::Initialize()
 {
-    // init SDL
+    // Init SDL
     int sdlResult = SDL_Init(SDL_INIT_VIDEO);
 
-    if ( sdlResult !=0 )
+    if (sdlResult != 0)
     {
         SDL_Log("SDL VIDEO INIT FAILED. %s",
-        SDL_GetError());
+                SDL_GetError());
         return false;
     }
 
     mWindow = SDL_CreateWindow(
         "Game Name goes here",
-        100,        // x coordinate
-        100,        // y coordinate
-        1024,       // window width
-        768,        // window height
-        0           // flags (0 is for no flags)
+        100,              // x coordinate
+        100,              // y coordinate
+        1024,             // Window width
+        768,              // Window height
+        SDL_WINDOW_OPENGL // Flags (0 is for no flags)
     );
 
     if (!mWindow)
     {
         SDL_Log("YOU (THE WINDOW) are A FAILURE, %s",
-        SDL_GetError());
+                SDL_GetError());
         return false;
     }
 
+    SDL_GetMouseState(      // Retrieve the current state of mouse
+        &mMousePosition.x,  // Mouse arrow coordinates on window
+        &mMousePosition.y); // & = dereferencing symbol: retrieves memory address
+
+    SDL_Log("Mouse position: x=%i y=%i", // Print mouse position
+            mMousePosition.x,
+            mMousePosition.y);
+
     mRenderer = SDL_CreateRenderer(
-        mWindow,        // window to create for the renderer
+        mWindow, // Window to create for the renderer
         -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-        );
-    
-    if(!mRenderer)
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (!mRenderer)
     {
         SDL_Log("Render = no go, %s",
-        SDL_GetError());
-        return false; 
+                SDL_GetError());
+        return false;
     }
     return true;
 }
 
 void Game::RunLoop()
 {
-    while(mIsRunning) {
+    while (mIsRunning)
+    {
         ProcessInput();
         UpdateGame();
         GenerateOutput();
@@ -65,13 +81,31 @@ void Game::ProcessInput()
                      // Reads from event queue
                      // Places events on event queue
 
-    while (SDL_PollEvent( &event ))
+    while (SDL_PollEvent(&event))
     {
         switch (event.type)
         {
         case SDL_QUIT:
             mIsRunning = false;
             break;
+        }
+
+        // Track mouse position
+        SDL_GetMouseState(      // Retrieve the current state of mouse
+            &mMousePosition.x,  // Mouse arrow coordinates on window
+            &mMousePosition.y); // & = dereferencing symbol: retrieves memory address
+
+        SDL_Log("Mouse position: x=%i y=%i", // Print mouse position
+                mMousePosition.x,
+                mMousePosition.y);
+
+        if (mMousePosition.x <= 100)
+        {
+            flag = false;
+        }
+        else
+        {
+            flag = true;
         }
     }
 
@@ -95,35 +129,66 @@ void Game::UpdateGame()
 
 void Game::GenerateOutput()
 {
-    SDL_SetRenderDrawColor(
-        mRenderer,
-        178,    // Red
-        102,    // Green
-        255,    // Blue
-        255     // Alpha (0=transparent 255=not)
-    );
+    if (flag == true)
+    {
+        SDL_SetRenderDrawColor(
+            mRenderer,
+            178, // Red
+            102, // Green
+            255, // Blue
+            255  // Alpha (0=transparent 255=not)
+        );
 
-    // Clear Back buffer
-    SDL_RenderClear(mRenderer);
+        // Clear Back buffer
+        SDL_RenderClear(mRenderer);
 
-    // Render the new output
-    SDL_SetRenderDrawColor(mRenderer, 140, 140, 140, 255);
+        // Render the new output
+        SDL_SetRenderDrawColor(mRenderer, 140, 140, 140, 255);
 
-    // Draw a top wall
-    SDL_Rect wall{
-        0,          // top left x
-        0,          // top teft y
-        1024,       // width
-        68          // height
-    };
-    SDL_RenderFillRect(mRenderer, &wall);
+        // Draw a top wall
+        SDL_Rect wall{
+            0,    // top left x
+            0,    // top teft y
+            1024, // width
+            68    // height
+        };
+        SDL_RenderFillRect(mRenderer, &wall);
 
-    // Draw a bottom wall
-    wall.y = 768 - 68;
-    SDL_RenderFillRect(mRenderer, &wall);
+        // Draw a bottom wall
+        wall.y = 768 - 68;
+        SDL_RenderFillRect(mRenderer, &wall);
 
-    // Swap main view to be now the back buffer
-    SDL_RenderPresent(mRenderer);
+        // SDL_Rect sidewall {
+        //     50,
+        //     50,
+        //     50,
+        //     10
+        // };
+        // SDL_RenderFillRect(mRenderer, &sidewall);
+
+        // Swap main view to be now the back buffer
+        SDL_RenderPresent(mRenderer);
+    }
+
+    else
+    {
+        SDL_SetRenderDrawColor(
+            mRenderer,
+            0, // Red
+            0, // Green
+            0, // Blue
+            0  // Alpha (0=transparent 255=not)
+        );
+
+        // Clear Back buffer
+        SDL_RenderClear(mRenderer);
+
+        // Render the new output
+        SDL_SetRenderDrawColor(mRenderer, 140, 140, 140, 255);
+
+        // Swap main view to be now the back buffer
+        SDL_RenderPresent(mRenderer);
+    }
 
     return;
 }
--- game/Game.h
+++ game/Game.h
@@ -20,9 +20,15 @@ private:
     void UpdateGame();
     void GenerateOutput();
 
+    // SDL_Point: A data stucture that defines a two dimensional point.
+    // Data Fields: int x, int y
+    SDL_Point mWindowPosition; // Position of window
+    SDL_Point mWindowSize;     // Size of window
+    SDL_Point mMousePosition;  // Mouse position coordinates
+
     // Member data
-    SDL_Window *mWindow;   // Window created by SDL2 Lib
+    SDL_Window *mWindow;     // Window created by SDL2 Lib
     SDL_Renderer *mRenderer; // Renderer for 2D drawing
-    Uint32 mTicksCount;    // Number of ticks since the start of the game
-    bool mIsRunning;       // Game should continue to run
+    Uint32 mTicksCount;      // Number of ticks since the start of the game
+    bool mIsRunning;         // Flag for if game should continue to run
 };
